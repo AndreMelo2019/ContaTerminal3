0607347d250d018bc4294da4a28ba1ac6565a328
AndreMelo2019 <59091264+AndreMelo2019@users.noreply.github.com>
2024-10-25 13:50:21 -0300

    Add files via upload

---
+++ ContaTerminal.java
@@ -0,0 +1,33 @@
+import javax.xml.bind.JAXBElement;
+
+public class ContaTerminal {
+    /**
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        //TODO: implementar a classe scanner;
+
+    
+
+       
+        int Numero  = 1021;
+        String Agencia  = "067-8";
+        String NomeCliente   = "Mario Andrade";
+        double Saldo = 237.48;
+        System.out.println(NomeCliente); (Numero);
+
+
+    }
+
+    @Override
+    public String toString() {
+        return "ContaTerminal []";
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        // TODO Auto-generated method stub
+        return super.clone();
+    }
+}
