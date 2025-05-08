c76ee245ef95d94f0e274e81c63c6dd353df74ac
AndreMelo2019 <59091264+AndreMelo2019@users.noreply.github.com>
2025-05-08 17:41:44 -0300

    Update ContaTerminal.java

--- ContaTerminal.java
+++ ContaTerminal.java
@@ -1,33 +1,16 @@
-import javax.xml.bind.JAXBElement;
+import java.util.Scanner;
 
 public class ContaTerminal {
-    /**
-     * @param args
-     * @throws Exception
-     */
+
     public static void main(String[] args) throws Exception {
-        //TODO: implementar a classe scanner;
-
-    
-
-       
         int Numero  = 1021;
         String Agencia  = "067-8";
         String NomeCliente   = "Mario Andrade";
         double Saldo = 237.48;
-        System.out.println(NomeCliente); (Numero);
 
-
-    }
-
-    @Override
-    public String toString() {
-        return "ContaTerminal []";
-    }
-
-    @Override
-    protected Object clone() throws CloneNotSupportedException {
-        // TODO Auto-generated method stub
-        return super.clone();
+        System.out.println("Nome do Cliente: " + NomeCliente);
+        System.out.println("Número da Conta: " + Numero);
+        System.out.println("Agência: " + Agencia);
+        System.out.println("Saldo: R$" + Saldo);
     }
 }
